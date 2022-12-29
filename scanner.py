b6ba13bce549aed0f6772ae52791a4da2af6ba32
mporada259449 <259449@student.pwr.edu.pl>
2022-12-29 12:58:23 +0100

    scanner cli

--- scanner.py
+++ scanner.py
@@ -1,11 +1,11 @@
 from scapy.all import *
-from random import randrange
+import argparse
 class Scanner():
     def dnsamplifcation(self, ip):
         query_type = ["ANY", "A", "AAAA", "CNAME", "MX"]
         #src_addr = ".".join([str(randrange(1, 255)) for _ in range(4)])
         for query in query_type:
-            dns_packet = IP(dst = ip)/UDP(dport=53)/DNS(rd =1, qd = DNSQR(qname = "google.com", qtype = query))
+            dns_packet = IP(dst = ip)/UDP(dport=53)/DNS(rd = 1, qd = DNSQR(qname = "google.com", qtype = query))
             """"pomysł na razie jest taki że prześle 100 pakietów na serwer jeśli dostne 100 odpowiedzi to znaczy że jest podatny, mogę też 
             policzyc do tego ratio response/request bo w sumie to daje dużą wartość"""
             for _ in range(30):
@@ -36,10 +36,10 @@ class Scanner():
                 print(f"Host {ip} jest podatny na atak dla mode={mode}")
                 print("Amplification ratio: ", len(result)/len(ntp_packet)) 
 
-    def tftpamplifcation(self, ip, filename):
+    def tftpamplifcation(self, ip):
         opcode_type = [1, 5]
         for opcode in opcode_type:
-            tftp_packet = IP(dst = ip)/UDP(dport=69)/TFTP(opcode=opcode, filename = filename)
+            tftp_packet = IP(dst = ip)/UDP(dport=69)/TFTP(op=opcode)
             for _ in range(30):
                 try:
                     result = sr1(tftp_packet, verbose = False, timeout=0.2)
@@ -53,11 +53,21 @@ class Scanner():
                 print("Amplification ratio: ", len(result)/len(tftp_packet))
 
 
-    def openrelay(self, ip):
-        pass
-
     
 if __name__=="__main__":
     scanner = Scanner()
-    scanner.dnsamplifcation("192.168.100.1")
+    parser = argparse.ArgumentParser(description="""Program pozwala na wykrywanie, czy serwer usługi jest podatny na atak UDP Amplification""")
+    parser.add_argument("-d", help= "Sprawdzenie serwera DNS", action="store_true")
+    parser.add_argument("-n", help="Sprawdzenie dla serwera NTP", action="store_true")
+    parser.add_argument("-t", help="Sprawdzenie dla serwera TFTP", action="store_true")
+    parser.add_argument("-a", help="Adres serwera lub domena", type=str, required=True)
+    args = parser.parse_args()
 
+    if args.d==args.n==args.t==False:
+        print("Nie podano rodzaju usługi")
+    if args.d==True:
+        scanner.dnsamplifcation(args.a)
+    if args.n==True:
+        scanner.ntpamplifcation(args.a)
+    if args.t==True:
+        scanner.tftpamplifcation(ip = args.a)
